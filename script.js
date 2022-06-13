cafa5e67e0f23c38eb70b74c59a56155ad786e62
Diego Fernandez <diegoj.fernandez@outlook.com>
2022-06-13 16:09:36 -0400

    Add operation functions to script.js

--- script.js
+++ script.js
@@ -1,11 +1,74 @@
-const numBtn = document.querySelectorAll(".num"); 
+const numBtn = document.querySelectorAll(".num");
+const operationBtn = document.querySelectorAll(".operation");
+const equal = document.querySelector(".equal");
+const clrBtn = document.querySelector(".clr");
 const screen = document.querySelector(".screen");
 const btnContainer = document.querySelector(".btn-container");
-const newP = document.createElement("p");
+const firstNumber = document.createElement("p");
+const operation = document.createElement("p");
+const secondNumber = document.createElement("p");
+const finalNumber = document.createElement("p");
 const eyeContainer = document.querySelector(".eye-container");
 const mouth = document.querySelector(".mouth");
 
+let ans;
 
+// CALCULATOR FUNCTIONS
+
+const add = function(num1,num2) {
+    if (num1 >= 0 && num2 >= 0) {
+      return num1 + num2;
+    }
+};
+  
+const subtract = function(num1,num2) {
+      return num1 - num2;
+};
+  
+const sum = function(array1) {
+      if (array1 == "") {
+      return 0;
+    } else if (array1.length == 1) {
+      return array1[array1.length - 1];
+    } else {
+      let initial = 0;
+      for (let i = 0; i < array1.length; i++) {
+        initial += array1[i];
+      }
+      return initial;
+    }
+};
+  
+const multiply = function(num1,num2) {
+    return num1 * num2;
+};
+
+const divide = function(num1,num2) {
+    return num1 / num2;
+};
+  
+  const power = function(num1,num2) {
+    let initial = num1;
+      for (let i = 0; i < num2 - 1; i++) {
+      initial *= num1;
+    }
+    return initial;
+};
+  
+const factorial = function(num1) {
+      if (num1 == 0 || num1 == 1) {
+      return 1;
+    } else {
+      let array1 = [];
+      for (let i = 1; i <= num1; i++) {
+        array1.push(i);
+      }
+      return multiply(array1);
+    }
+};
+
+
+// DOM MANIPULATION
 
 btnContainer.addEventListener("mousedown", () => {
     if (screen.contains(eyeContainer) || screen.contains(mouth)) {
@@ -20,16 +83,68 @@ btnContainer.addEventListener("mousedown", () => {
 
 numBtn.forEach(function (i) {
     i.addEventListener("click", () => {
-        if (!screen.contains(newP)) {
-            newP.setAttribute("class", "input");
-            newP.innerText = i.innerText;
-            screen.append(newP);
-        } else {
-            if (newP.innerText.length >= 10) {
-                return;
+        if (!screen.contains(operation)) {
+            if (!screen.contains(firstNumber)) {
+                firstNumber.setAttribute("class", "input");
+                firstNumber.innerText = i.innerText;
+                screen.append(firstNumber);
             } else {
-                newP.innerText = newP.innerText + i.innerText;
+                if (firstNumber.innerText.length >= 10) {
+                    return;
+                } else {
+                    firstNumber.innerText = firstNumber.innerText + i.innerText;
+                }
+            }
+        } else {
+            if (!screen.contains(secondNumber)) {
+                operation.classList.remove("input");
+                secondNumber.setAttribute("class", "input");
+                secondNumber.innerText = i.innerText;
+                screen.append(secondNumber);
+            } else {
+                if (secondNumber.innerText.length >= 10) {
+                    return;
+                } else {
+                    secondNumber.innerText = secondNumber.innerText + i.innerText;
+                }
             }
         }
     });
 });
+
+clrBtn.addEventListener("click", () => {
+    screen.innerText = "";
+});
+
+operationBtn.forEach(function(i) {
+    i.addEventListener("click", () => {
+        firstNumber.classList.remove("input");
+        operation.setAttribute("class", "input");
+        operation.innerText = i.innerText;
+        screen.append(operation);
+    });
+});
+
+equal.addEventListener("click", () => {
+    secondNumber.classList.remove("input");
+    console.log(firstNumber);
+    console.log(secondNumber);
+    ans = operate(Number(firstNumber.innerText), Number(secondNumber.innerText));
+    console.log(ans);
+    finalNumber.setAttribute("class", "input");
+    finalNumber.setAttribute("id", "answer");
+    finalNumber.innerText = ans;
+    screen.append(finalNumber);
+});
+
+let operate = (num1,num2) => {
+    if (operation.innerText == "+") {
+        return add(num1,num2);
+    } else if (operation.innerText == "-") {
+        return subtract(num1,num2);
+    } else if (operation.innerText == "x") {
+        return multiply(num1,num2);
+    }else if (operation.innerText == "/") {
+        return divide(num1,num2);
+    }
+}
