ebf8da2c1af685ae0a2ac50c35bb5d13804f8dc1
Diego Fernandez <diegoj.fernandez@outlook.com>
2022-06-13 12:08:29 -0400

    Allow first number input and add size limit to input

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
 const numBtn = document.querySelectorAll(".num"); 
 const screen = document.querySelector(".screen");
 const btnContainer = document.querySelector(".btn-container");
+const newP = document.createElement("p");
+const eyeContainer = document.querySelector(".eye-container");
+const mouth = document.querySelector(".mouth");
 
 
-btnContainer.addEventListener("mouseover", () => {
-    screen.style.background = `#000000`;
-    screen.style.color = `#33ff00`;
+
+btnContainer.addEventListener("mousedown", () => {
+    if (screen.contains(eyeContainer) || screen.contains(mouth)) {
+        screen.style.background = `#000000`;
+        screen.style.color = `#33ff00`;
+        eyeContainer.remove();
+        mouth.remove();
+        screen.style.display = `block`
+        screen.style.padding = `10px`
+    }
 });
 
 numBtn.forEach(function (i) {
     i.addEventListener("click", () => {
-        screen.innerText = i.innerText;
+        if (!screen.contains(newP)) {
+            newP.setAttribute("class", "input");
+            newP.innerText = i.innerText;
+            screen.append(newP);
+        } else {
+            if (newP.innerText.length >= 10) {
+                return;
+            } else {
+                newP.innerText = newP.innerText + i.innerText;
+            }
+        }
     });
 });
